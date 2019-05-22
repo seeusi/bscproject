41fa1c785798e15a1fa06c1a0e7045a35f102e90
seeusi <sso216@ic.ac.uk>
2019-05-22 10:13:07 +0100

    untracked changed

--- mywork/datagen.py
+++ mywork/datagen.py
@@ -13,21 +13,21 @@ import csv
 eachline = []
 
 for num in range (500):
-    parameter1 = round(random.uniform(10,20), 3)
+    parameter1 = round(random.uniform(10,200), 3)
     parameter2 = round(random.uniform(20,200), 3)
     parameter3 = round((parameter1 * parameter1) + (2 * parameter2), 1)
-    parameter4 = round(random.uniform(0,5), 3)
+    parameter4 = round(random.uniform(0,500), 3)
     parameter5 = round(random.uniform(50, 500), 3)
-    parameter6 = round(random.uniform(30, 50), 3)
-    parameter7 = round((parameter5 - (parameter4 * parameter6)), 1)
+    parameter6 = round(random.uniform(30, 500), 3)
+    parameter7 = round((parameter5 - parameter4), 1)
     parameter8 = round(random.uniform(100,200), 3)
-    parameter9 = round(random.uniform(2,20), 3)
-    parameter10 = round((parameter8 / parameter9 / 10), 1)
+    parameter9 = round(random.uniform(2,200), 3)
+    parameter10 = round((parameter8 / parameter9) * 10, 1)
     eachitem = [parameter1, parameter2, parameter3, parameter4, parameter5,
                 parameter6, parameter7, parameter8, parameter9, parameter10]
     eachline.append(eachitem)
 
-with open("sampledata.csv", "w") as datafile:
+with open("sampleData.csv", "w") as datafile:
     mywriter = csv.writer(datafile)
     mywriter.writerow(["Variable1", "Variable2", "Variable 3", "Variable 4",
                         "Variable 5", "Variable 6", "Variable 7", "Variable 8",
